330658cfb5ec802d2bd0ae80b659b3e588638f06
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-14 14:54:00 +0000

    Deploying to pages from @ localizethedocs/vtk-docs-l10n@26cf106ee41503cf3b696ed0577af84e48345d54 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+    ["9",       "Release 9.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/vtk-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/vtk-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/vtk-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/vtk-docs-l10n"],
+  ]
+};
